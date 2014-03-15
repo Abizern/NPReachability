8ba3e3daa682ea337f51da12fb7cbec71680d9a2
Abizer Nasir <abizern@abizern.org>
2014-03-15 18:42:08 +0000

    Improve documentation in the header

--- NPReachability.h
+++ NPReachability.h
@@ -14,38 +14,132 @@
 //  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 
+/**
+ NPReachability is an evolution of Apple's Reachability class that provides information about the network status.
+
+ As well as supporting the original's Notification based monitoring, this class supports both KVO and Blocks, so you can choose whichever way of handling changes as your application requires.
+
+ This class is written as a singleton, so be sure to reference it as
+
+ NPReachability *reachability = [NPReachability sharedInstance];
+
+ Make sure you maintain a strong reference to at least one object of this class or else ARC will clean it up underneath you.
+
+ ## Block support
+
+ Handlers are declared as
+
+ typedef void (^ReachabilityHandler)(NPReachability *curReach);
+
+ This takes the NPReachability object as a parameter. As originally written this class passed the `SCNetworkReachabilityFlags` as a parameter, but you can get that and more by messaging the object directly
+
+ You add blocks to be executed when the network status changes by using:
+
+ - (id)addHandler:(ReachabilityHandler)handler;
+
+ This returns an opaque object which you should use to remove the handler at the appropriate time (in a `dealloc`, say) with:
+
+ - (void)removeHandler:(id)opaqueObject;
+
+ ## KVO support
+
+ Two properties can observed for changes to the network status:
+
+ @property (assign, readonly, nonatomic, getter=isCurrentlyReachable) BOOL currentlyReachable;
+ @property (assign, readonly, nonatomic) SCNetworkReachabilityFlags currentReachabilityFlags;
+
+ ## NSNotification
+
+ When the network status changes a `NPReachabilityChangedNotification` is sent with the NPReachability instance as the notification object.
+
+ ## Dependencies
+
+ - Xcode 5.0+ for ARC support, automatic synthesis and compatibility libraries. This might work for Xcode 4.2+, but I haven't been able to test it.
+ - The SystemConfiguration Framework should be added to your projects
+
+ Please use and improve!
+ */
+
 #import <Foundation/Foundation.h>
 #import <SystemConfiguration/SystemConfiguration.h>
+
 @class NPReachability;
+
+/**
+ The Name of the NSNotification that is  posted when the reachability changes
+ */
 extern NSString * const NPReachabilityChangedNotification;
 
-// Handler for changes in reachability.
+/**
+ Definition of the handler block that is performed on changes to reachability
+
+ @param curReach the current NPReachability instance
+ */
 typedef void (^ReachabilityHandler)(NPReachability *curReach);
 
+/**
+ An enum for the Network status
+ */
 typedef enum {
 	NPRNotReachable = 0,
 	NPRReachableViaWiFi,
 	NPRReachableViaWWAN
 } NPRNetworkStatus;
 
+
 @interface NPReachability : NSObject 
 
-// Allows KVO for `currentlyReachable` and `currentReachabilityFlags`
+/**
+ Whether or not the network is currently reachable. KVO compliant.
+ */
 @property (nonatomic, readonly, getter=isCurrentlyReachable) BOOL currentlyReachable;
+
+/**
+ The SystemConfiguration reachability flags. KVO compliant.
+ */
 @property (nonatomic, readonly) SCNetworkReachabilityFlags currentReachabilityFlags;
+
+/**
+ The current network status. KVO compliant.
+ */
 @property (nonatomic, readonly) NPRNetworkStatus currentNetworkStatus;
 
-// Singleton initialiser
+/**
+ The singleton initialiser.
+ 
+ Be sure to maintain at least one strong reference to an instance to prevent it being released out from under you.
+
+ @return A singleton instance of NPReachability
+ */
 + (NPReachability *)sharedInstance;
 
-// A handy class method:
+/**
+ A convenience class method for SCNetworkReachabilityFlags for whether or not they indicate a connection is available
+
+ @param flags The flags to test against.
+
+ @return YES if a connection is available, NO otherwise.
+ */
 + (BOOL)isReachableWithFlags:(SCNetworkReachabilityFlags)flags;
 
 
-// Returns an opaque object used for removal later. Caution: this copies the 
-// block. If the block retains an object, you may end up with a retain cycle.
-// In that case, consider using KVO or NSNotifications instead.
+/**
+ Add a handler block to be run when reachability changes.
+ 
+ Strong references are kept to objects in the handler block, so be careful to avoid retain cycles.
+
+ @param handler A ReachabilityHandler block that takes one parameter, the instance of the NPReachability class.
+
+ @return An opaque object that is used to remove the handler when it is no longer required.
+ */
 - (id)addHandler:(ReachabilityHandler)handler;
+
+/**
+ Removes a reachability changed handler from the object
+
+ @param opaqueObject The opaque object that was returned by the call `addHandler:` that was used when adding the handler block.
+ */
+
 - (void)removeHandler:(id)opaqueObject;
 
 
